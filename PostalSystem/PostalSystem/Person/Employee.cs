06acf790f1a22411205b1626648db2dde6364d5a
nikolovdeyan <deyan.nikolov@gmail.com>
2017-01-11 09:29:33 +0200

    Class structure suggestion

---
+++ PostalSystem/PostalSystem/Person/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalSystem
+{
+    public class Employee : Person
+    {
+        public override void Meet(Person other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PostalSystem/PostalSystem/Person/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalSystem
+{
+    public abstract class Person
+    {
+        /// <summary>
+        /// Meet another person for an interaction/exchange - fire off events...
+        /// </summary> 
+        ///public abstract void Meet(Person other);
+    }
+}
