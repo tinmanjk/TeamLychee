25e5dca02b23ea32bb07b32231fce638143be67a
vixataaa <vmetodiev93@gmail.com>
2017-01-14 20:10:23 +0200

    another post system suggestion

---
+++ PostSystem/PostSystem/Shipments/Parcel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSystem.Structs;
+using PostSystem.People;
+using PostSystem.PostOffices;
+using PostSystem.Enums;
+
+namespace PostSystem.Shipments
+{
+    class Parcel : Shipment
+    {
+        private Dimensions dimensions;
+        private string content;
+
+        //properties
+
+        //constructors
+        public Parcel(Sender sender, Receiver receiver, PostOffice sentFrom, PostOffice sentTo, DeliveryType deliveryType, Dimensions dimensions, string content)
+            :base(sender, receiver, sentFrom, sentTo, deliveryType)
+        {
+            this.dimensions = dimensions;
+            this.content = content;
+        }
+
+        
+    }
+}
