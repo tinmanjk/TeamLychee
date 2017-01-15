33cc3999739d861cdb92663006cad8435ee8238a
nikolovdeyan <deyan.nikolov@gmail.com>
2017-01-15 18:05:17 +0200

    Adding a simple engine for postal services

---
+++ PostingServices/PostingServices.ConsoleClient/PostEngine.cs
@@ -0,0 +1,65 @@
+﻿namespace PostEngineUtility
+{
+    using System;
+    using System.Text;
+
+    public static class PostEngine
+    {
+
+        public static void StartUp(DateTime timestamp)
+        {
+            DrawScreen(timestamp);
+        }
+
+        public static void DrawScreen(DateTime timestamp)
+        {
+            StringBuilder sbHeader = new StringBuilder();
+            StringBuilder sbFooter = new StringBuilder();
+
+            // Warning! Ugly code here...
+            // Header
+            sbHeader.AppendLine(new String('#', 76))
+              .AppendFormat("{0}{1,15}{0}", new String('#', 30), " POSTAL SERVICE ")
+              .Append("\n")
+              .AppendLine(new String('#', 76));
+
+            sbHeader.AppendLine()
+              .AppendFormat("{0,30}{1}", "Today is ", timestamp.ToString("dd.MM.yyyy -- dddd"))
+              .Append("\n")
+              .AppendFormat("{0,30}{1}", "        Time: ", timestamp.ToString("HH:mm"));
+
+            Console.WriteLine();
+            Console.WriteLine(sbHeader.ToString());
+
+            // Here be log
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Footer
+            sbFooter.AppendLine(new String('_', 76))
+                    .AppendLine(" TIME CONTROLS: \n")
+                    .AppendLine("         * Press 'N' to submit a new Order")
+                    .AppendLine("         * Press '->' to step forward")
+                    .AppendLine("         * Press '+' to increase step TimeSpan (by 3 hours)")
+                    .AppendLine("         * Press '-' to decrease step TimeSpan (by 3 hours)")
+                    .AppendLine("         * Press 'Q' to exit")
+                    .Append("\n")
+                    .AppendLine(new String('#', 76));
+
+            Console.WriteLine(sbFooter.ToString());
+
+        }
+
+        public static void UpdateScreen(DateTime timestamp)
+        {
+            Console.Clear();
+            DrawScreen(timestamp);
+        }
+    }
+}
--- PostingServices/PostingServices.ConsoleClient/Program.cs
+++ PostingServices/PostingServices.ConsoleClient/Program.cs
@@ -1,21 +1,51 @@
-﻿using System;
-using PostingServices.People;
-using PostingServices.PostOffices;
-using PostingServices.Core.Models.Shipment.Abstract;
-using PostingServices.Core.Models.Shipments;
-using Infrastructure.Enumerations;
-using PostingServices.Primitives.Address;
-using PostingServices.Orders;
-using PostingServices.Primitives.Location;
-using Infrastructure.Constants;
-using System.Device.Location;
-
-namespace PostingServices
+﻿namespace PostingServices
 {
+    using System;
+    using TimeWalkerUtility;
+    using PostEngineUtility;
+
     class Program
     {
         static void Main()
         {
+            ConsoleKeyInfo keyinfo;
+            TimeWalker tw = new TimeWalker();
+
+
+            PostEngine.StartUp(tw.GetCurrentTime());
+
+            do
+            {
+                keyinfo = Console.ReadKey();
+                if (keyinfo.Key == ConsoleKey.RightArrow)
+                {
+                    tw.StepForward();
+                    PostEngine.UpdateScreen(tw.GetCurrentTime());
+                }
+
+                if (keyinfo.Key == ConsoleKey.Add)
+                {
+                    tw.MoveTimeFaster();
+                    PostEngine.UpdateScreen(tw.GetCurrentTime());
+                }
+
+                if (keyinfo.Key == ConsoleKey.Subtract)
+                {
+                    tw.MoveTimeSlower();
+                    PostEngine.UpdateScreen(tw.GetCurrentTime());
+                }
+
+                if (keyinfo.Key == ConsoleKey.N)
+                {
+                    tw.MoveTimeSlower();
+                    PostEngine.UpdateScreen(tw.GetCurrentTime());
+                }
+
+            } while (keyinfo.Key != ConsoleKey.Q);
+
+
+
+
             // post offices are defined manually, can be kept in a file (might not be the best option)
             // new sender
             // new receiver
@@ -37,27 +67,27 @@ namespace PostingServices
             // * can add a few more types of shipments in order to get the required number of classes/depth of inheritance
             // *         
 
-            Sender sender = new Sender("Gosho Sender", "+359888888");
-            Address receiverAddress = new Address("Sofia", "Gorno nanadolnishte", 10);
-            Receiver receiver = new Receiver("Gosho", "+88888+", receiverAddress);
+            //Sender sender = new Sender("Gosho Sender", "+359888888");
+            //Address receiverAddress = new Address("Sofia", "Gorno nanadolnishte", 10);
+            //Receiver receiver = new Receiver("Gosho", "+88888+", receiverAddress);
 
-            PostOffice officeSofia = new PostOffice("HQ Sofia", CityLocations.sofia);
-            PostOffice officeBurgas = new PostOffice("HQ Burgas", CityLocations.burgas);
-            string text = "Hope this thing works";
+            //PostOffice officeSofia = new PostOffice("HQ Sofia", CityLocations.sofia);
+            //PostOffice officeBurgas = new PostOffice("HQ Burgas", CityLocations.burgas);
+            //string text = "Hope this thing works";
 
-            Shipment shipment1 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.TheFlash, PaperSize.A4, text);
-            Shipment shipment2 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.Regular, PaperSize.A4, text);
-            Shipment shipment3 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.Priority, PaperSize.A4, text);
-            sender.SendShipment(shipment1);
-            sender.SendShipment(shipment2);
-            sender.SendShipment(shipment3);
+            //Shipment shipment1 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.TheFlash, PaperSize.A4, text);
+            //Shipment shipment2 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.Regular, PaperSize.A4, text);
+            //Shipment shipment3 = new Letter(sender, receiver, officeSofia, officeBurgas, DeliveryType.Priority, PaperSize.A4, text);
+            //sender.SendShipment(shipment1);
+            //sender.SendShipment(shipment2);
+            //sender.SendShipment(shipment3);
 
-            Console.WriteLine("Orders:");
-            foreach (var item in OrdersContainer.Orders)
-            {
-                Console.WriteLine(item);
-            }
-            
+            //Console.WriteLine("Orders:");
+            //foreach (var item in OrdersContainer.Orders)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
             //Shipment shipmentToSend = new Shipment(sender, receiver, ...)
         }
     }
